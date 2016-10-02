20796d77d061b95bb24e54e9f48c5a0f0f3a120e
sdhz150 <yanghui140@gmail.com>
2016-10-02 13:11:36 +0800

    添加状态栏运营商

--- config/telephony.mk
+++ config/telephony.mk
@@ -2,6 +2,10 @@
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
 # Telephony packages
 PRODUCT_PACKAGES += \
     messaging \
